bd35f192d88fcc320a67192970274a6d5b70eb4c
Júlia <86375053+juliabsommer@users.noreply.github.com>
2022-01-12 15:35:42 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Espaço Padel</Text>
+      <Text>Criado por Júlia Sommer</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightgreen',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
